8782929f5454901d75d63c185174757850ade3dc
Sowbarnyam <sowbarnya@qaoncloud.com>
2024-12-21 15:32:37 +0530

    Webresponsive.cy.js

--- Webresponsive.cy.js
+++ Webresponsive.cy.js
@@ -3,8 +3,8 @@ import LoginPage from "./POM PageClasses.cy.js/LoginPage.cy"
 describe('Response',function(){
 it('WebpageResponse',function(){
 cy.visit('https://adactinhotelapp.com/')
-cy.get("#username").type("PreethiSri")
-cy.get("#password").type("pree@0212")
+cy.get("#username").type("lashu")
+cy.get("#password").type("lashu0212")
 cy.get("#login").click()
 cy.title().should('eq','Adactin.com - Search Hotel')
 
@@ -15,8 +15,8 @@ it('Viewport', function(){
     const login = new LoginPage  // object of the POM class
     cy.viewport('iphone-6+')     // get this view option from viewport document from cypress.io
     login.visit()
-    login.userName("PreethiSri")
-    login.passWord("pree@0212")
+    login.userName("lashu")
+    login.passWord("lashu")
     login.loginButton()
     cy.title().should('eq','Adactin.com - Search Hotel')
 })
