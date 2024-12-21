5266ba81cca950caeeb4bddc5bf218d59bb0eb52
Sowbarnyam <sowbarnya@qaoncloud.com>
2024-12-21 15:33:34 +0530

    POM test.cy.js

--- POM test.cy.js
+++ POM test.cy.js
@@ -4,8 +4,8 @@ describe('POM TestClass', function(){
     it('Adactin app', function(){
         const login = new LoginPage  // object of the class
         login.visit()
-        login.userName("PreethiSri")
-        login.passWord("pree@0212")
+        login.userName("lashu")
+        login.passWord("lashu")
         login.loginButton()
 
         cy.title().should('be.equal','Adactin.com - Search Hotel')
